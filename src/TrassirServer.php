1a591ad8fce1e58fdbb0f7643c16e8cc48f17274
Denis Glushakov <denis.glushakov@bk.ru>
2018-06-25 14:37:45 +0300

    some fixes

--- src/TrassirServer.php
+++ src/TrassirServer.php
@@ -221,9 +221,9 @@ class TrassirServer
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
         $content = curl_exec($curl);
         curl_close($curl);
-        if (file_exists($path)) :
+        if (file_exists($path)){
             unlink($path);
-        endif;
+        }
         $fp = fopen($path, 'x');
         if ($fp) {
             fwrite($fp, $content);
